6b0b6875849b4af43e98fbf67615549882d01f4d
Micheal kuo <sxdcfv20002000@gmail.com>
2021-10-01 12:08:36 +0800

    “1001PM00“

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import RadioButton from './components/RadioButton'
+import CheckBox from './components/CheckBox'
 
 function App(props) {
   const [inputText, setInputText] = useState('')
@@ -6,6 +8,16 @@ function App(props) {
   const [selectedOption, setSelectedOption] = useState('')
   // radio - 狀態記錄是被選中選項按鈕的值
   const [gender, setGender] = useState('')
+  // radio - 專用元件
+  const [gender2, setGender2] = useState('')
+  const genderOptions = ['男', '女', '不提供', '不確定']
+  // checkbox - single
+  const [agree, setAgree] = useState(false)
+  // checkbox - group
+  const [likeList, setLikeList] = useState([])
+  // checkbox - group - 專用元件
+  const [likeList2, setLikeList2] = useState([])
+  const fruitOptions = ['芒果', '西瓜', '芭樂']
 
   return (
     <>
@@ -31,8 +43,6 @@ function App(props) {
       </section>
       <section id="selectedOption">
         <h2>下拉選單</h2>
-        {/* 第一個執會使用state的初始值  把要selected放入 useState =("這" ) */}
-
         <select
           value={selectedOption}
           onChange={(e) => {
@@ -78,6 +88,107 @@ function App(props) {
         />
         <label>未定</label>
         <br />
+      </section>
+      <section id="radioButton">
+        <h2>選項按鈕(專用元件)</h2>
+        {genderOptions.map((v, i) => {
+          return (
+            <RadioButton
+              key={i}
+              value={v}
+              checkedValue={gender2}
+              setCheckedValue={setGender2}
+            />
+          )
+        })}
+      </section>
+      <section id="checkboxSingle">
+        <h2>單一核取方塊</h2>
+        <input
+          type="checkbox"
+          checked={agree}
+          onChange={(e) => {
+            setAgree(e.target.checked)
+          }}
+        />
+        <label>我同意網站註冊規定</label>
+      </section>
+      <section id="checkboxGroup">
+        <h2>多個核取方塊</h2>
+        <input
+          type="checkbox"
+          value="芒果"
+          checked={likeList.includes('芒果')}
+          onChange={(e) => {
+            // toggle(切換)
+            // 如果這選項 在 陣列中 -> 移出陣列
+            if (likeList.includes(e.target.value)) {
+              //1. 拷貝新陣列
+              //2. 新陣列中作處理
+              const newLikeList = likeList.filter((v, i) => {
+                return v !== e.target.value
+              })
+
+              // for迴圈語法
+              // const newLikeList = []
+              // for (let i = 0; i < likeList.length; i++) {
+              //   if (likeList[i] !== e.target.value) {
+              //     newLikeList.push(likeList[i])
+              //   }
+              // }
+
+              //3. 設定回狀態
+              setLikeList(newLikeList)
+            } else {
+              // 如果這選項 不在 陣列中 -> 加入陣列
+              //1. 拷貝新陣列
+              //2. 新陣列中作處理
+              const newLikeList = [...likeList, e.target.value]
+              //3. 設定回狀態
+              setLikeList(newLikeList)
+            }
+          }}
+        />
+        <label>芒果</label>
+        <input
+          type="checkbox"
+          value="西瓜"
+          checked={likeList.includes('西瓜')}
+          onChange={(e) => {
+            // toggle(切換)
+            // 如果這選項 在 陣列中 -> 移出陣列
+            if (likeList.includes(e.target.value)) {
+              //1. 拷貝新陣列
+              //2. 新陣列中作處理
+              const newLikeList = likeList.filter((v, i) => {
+                return v !== e.target.value
+              })
+
+              //3. 設定回狀態
+              setLikeList(newLikeList)
+            } else {
+              // 如果這選項 不在 陣列中 -> 加入陣列
+              //1. 拷貝新陣列
+              //2. 新陣列中作處理
+              const newLikeList = [...likeList, e.target.value]
+              //3. 設定回狀態
+              setLikeList(newLikeList)
+            }
+          }}
+        />
+        <label>西瓜</label>
+      </section>
+      <section id="checkboxGroup2">
+        {fruitOptions.map((v, i) => {
+          return (
+            <CheckBox
+              key={i}
+              value={v}
+              checkedValueList={likeList2}
+              setCheckedValueList={setLikeList2}
+            />
+          )
+        })}
       </section>
     </>
   )
