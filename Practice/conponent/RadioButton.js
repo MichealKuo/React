9c990791afaa1fcdf4bdc62f24a06701cbf0d92e
Micheal kuo <sxdcfv20002000@gmail.com>
2021-10-04 08:52:10 +0800

    “1004AM09“

--- Practice/conponent/RadioButton.js
+++ Practice/conponent/RadioButton.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 function RadioButton(props) {
   const { value, checkedValue, setCheckedValue } = props
+
   return (
     <>
       <input
         type="radio"
-        value="value"
-        checked={checkedValue === 'value'}
+        value={value}
+        checked={checkedValue === value}
         onChange={(e) => {
           setCheckedValue(e.target.value)
         }}
