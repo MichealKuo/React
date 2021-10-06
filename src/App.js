a30078120603636bdb0c7d528411ae8c6f79d955
Micheal kuo <sxdcfv20002000@gmail.com>
2021-10-06 16:16:47 +0800

    “1006PM04“

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ function App() {
 
   return (
     <>
-      <Counter start={start} />
-      <button
-        onClick={() => {
-          setStart(111)
-        }}
-      >
-        start to 111
+      <button type="button" className="btn btn-primary">
+        Primary
       </button>
-      <button
-        onClick={() => {
-          setStart(999)
-        }}
-      >
-        start to 999
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-light">
+        Light
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button>
+
+      <button type="button" className="btn btn-link">
+        Link
       </button>
     </>
   )
